17266d95b0e855b007b9c440b05918fc24febc1e
Relena <zhaol3@illinois.edu>
2023-04-01 01:18:24 -0500

    new comment

--- my-app/src/Pages/NavBar.js
+++ my-app/src/Pages/NavBar.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 
 // this page serves as a helper page just so I can navigate to pages while trying to figure things out
 // need to be updated/deleted later
+// this is just a comment testing for nothing :)
 export const NavBar = () => {
     return (
         <div>
